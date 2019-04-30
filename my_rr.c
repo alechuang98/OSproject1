589dfab9ccf2147f8000ecd1c95eab26fe57c7bf
alechuang98 <alechuang98@gmail.com>
2019-04-30 19:50:04 +0800

    fix rr

--- my_rr.c
+++ my_rr.c
@@ -29,13 +29,13 @@ void my_rr(TSK *tsk, int n) {
 	for (int i = 0; i < n; i++) {
 		buf[i].tsk = tsk[i];
 		buf[i].id = i;
-		buf[i].ll = buf[i].rr = -1;
 	}
+	int id[1005];
 	qsort(buf, n, sizeof(TSK_rr), rr_cmp);
 	int t = 0;
-	int ptr = 0, st = -1, ed = -1;
-	while (ptr != n || st != -1) {
-		if (st == -1) {
+	int ptr = 0, ll = 0, rr = n-1, num = 0;
+	while (ptr != n || num) {
+		if (!num) {
 			int tt = buf[ptr].tsk.arr - t;
 			t = buf[ptr].tsk.arr;
 			for (int i = 0; i < tt; i++) {
@@ -43,47 +43,37 @@ void my_rr(TSK *tsk, int n) {
 				for (k = 0; k < 1000000UL; k++);
 			}
 			make(&tsk[buf[ptr].id]);
-			st = ed = ptr;
-			buf[ptr].ll = buf[ptr].rr = -1;
+			rr++; if (rr == n) rr = 0;
+			id[rr] = ptr;
 			ptr++;
+			num++;
 		}
-		int p = st;
-		while (p != -1) {
-			int la = tsk[buf[p].id].rem;
-			if (la > 500) la = 500;
-			while (ptr != n && t + la >= tsk[buf[ptr].id].arr) {
-				int used = tsk[buf[ptr].id].arr - t;
-				if (used) {
-					run(&tsk[buf[p].id], used);
-					la -= used;
-					t += used;
-				}
-				make(&tsk[buf[ptr].id]);
-				buf[ed].rr = ptr;
-				buf[ptr].ll = ed;
-				ed = ptr;
-				buf[ptr].rr = -1;
-				ptr++;
+		int p = id[ll];
+		int la = tsk[buf[p].id].rem;
+		if (la > 500) la = 500;
+		while (ptr != n && t + la >= tsk[buf[ptr].id].arr) {
+			int used = tsk[buf[ptr].id].arr - t;
+			if (used) {
+				run(&tsk[buf[p].id], used);
+				la -= used;
+				t += used;
 			}
-			if (la) { 
-				run(&tsk[buf[p].id], la);
-				t += la;
-			}
-			if (!tsk[buf[p].id].rem) {
-				if (st == ed) {
-					st = ed = -1;
-				} else if (p == st) {
-					st = buf[p].rr;
-					buf[st].ll = -1;
-				} else if (p == ed) {
-					ed = buf[p].ll;
-					buf[ed].rr = -1;
-				} else {
-					buf[buf[p].ll].rr = buf[p].rr;
-					buf[buf[p].rr].ll = buf[p].ll;
-				}
-			}
-			p = buf[p].rr;
+			make(&tsk[buf[ptr].id]);
+			rr++; if (rr == n) rr = 0;
+			id[rr] = ptr;
+			ptr++;
+			num++;
+		}
+		if (la) { 
+			run(&tsk[buf[p].id], la);
+			t += la;
+		}
+		ll++; if (ll == n) ll = 0;
+		num--;
+		if (tsk[buf[p].id].rem) {
+			rr++; if (rr == n) rr = 0;
+			id[rr] = p;
+			num++;
 		}
 	}
 }
--- my_rr.h
+++ my_rr.h
@@ -2,7 +2,7 @@
 #define MYRR_H
 typedef struct rr_process {
 	TSK tsk;
-	int id, ll, rr;
+	int id;
 } TSK_rr;
 int rr_cmp(const void *p1, const void *p2);
 void my_rr(TSK *tsk, int n);
